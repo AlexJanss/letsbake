c00c3520639a70feb5e48aa926f45c21fa0ccfe5
AlexJanss <alex.janssen20@gmail.com>
2020-12-06 18:44:33 +0100

    add fonts and font configs

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 import { default as theme } from './assets/theme.json';
+import { default as mapping } from './mapping.json';
 
 const HomeScreen = () => (
     <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
@@ -10,7 +11,10 @@ const HomeScreen = () => (
 );
 
 export default () => (
-    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
+    <ApplicationProvider
+        {...eva}
+        theme={{ ...eva.dark, ...theme }}
+        customMapping={mapping}>
       <HomeScreen />
     </ApplicationProvider>
 );
