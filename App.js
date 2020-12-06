27aa7ced9d9e0a42dc3670de23f8167a6cf5750b
AlexJanss <alex.janssen20@gmail.com>
2020-12-06 19:18:09 +0100

    WIP navigation

--- App.js
+++ App.js
@@ -1,33 +1,19 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { AppNavigator } from './components/navigation.component';
 import { default as theme } from './assets/theme.json';
 import { default as mapping } from './mapping.json';
 
-const HomeScreen = () => (
-    <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text category='h1'>Let's bake!</Text>
-    </Layout>
+export default () => (
+    <>
+        <IconRegistry icons={EvaIconsPack}/>
+        <ApplicationProvider    {...eva}
+                                theme={{...eva.dark, ...theme}}
+                                customMapping={mapping}>>
+            <AppNavigator/>
+        </ApplicationProvider>
+    </>
 );
 
-
-const Stack = createStackNavigator();
-
-function App() {
-    return (
-        <ApplicationProvider
-            {...eva}
-            theme={{...eva.dark, ...theme}}
-            customMapping={mapping}>
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen name="Home" component={HomeScreen}/>
-                </Stack.Navigator>
-            </NavigationContainer>
-        </ApplicationProvider>
-    );
-}
-
-export default App;
